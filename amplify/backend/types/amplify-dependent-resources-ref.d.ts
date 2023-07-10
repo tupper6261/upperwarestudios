8cebd4faaae2615d3741dc1f78e0420bf2b7cda0
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-07-10 15:11:34 -0400

    Relaunch

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,17 +6,6 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIKeyOutput": "string"
     }
   },
-  "auth": {
-    "upperwarestudios1b7c3f52": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "storage": {
     "imagestorage": {
       "BucketName": "string",
---
+++ src/skylab/SkylabLandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SkylabLandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Skylab</h1>
+      <p>Hello, world! This is the landing page for Skylab.</p>
+      {/* Add more content or sections as needed */}
+    </div>
+  );
+}
+
+export default SkylabLandingPage;
