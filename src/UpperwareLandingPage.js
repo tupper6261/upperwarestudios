56ecc16835a47c1fe5ec28faecd465ce16fd2e41
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-07-12 13:37:17 -0400

    Update UpperwareLandingPage.js

---
+++ src/UpperwareLandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function UpperwareLandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Upperware Studios</h1>
+      <p>Hello, world! This is the landing page for Upperware Studios.</p>
+      {/* Add more content or sections as needed */}
+    </div>
+  );
+}
+
+export default UpperwareLandingPage;
