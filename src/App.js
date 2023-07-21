4b0d1ee5d561fbf8f3d55f2234c793f027aa5258
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-07-21 09:53:51 -0400

    add resume.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import UpperwareLandingPage from './UpperwareLandingPage';
 import SkylabLandingPage from './skylab/SkylabLandingPage';
 import Meetups from './skylab/Meetups';
+import Resume from './Resume';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<UpperwareLandingPage />} />
+        <Route path="/resume" element={<Resume />} />
         <Route path="/skylab" element={<SkylabLandingPage />} />
         <Route path="/skylab/meetups" element={<Meetups />} />
         <Route path="/skylab/*" element={<SkylabLandingPage />} />
