2ebac596bc1d3b6f060a3c4ec78b5d8a9669b62f
tmupper <129107684+tupper6261@users.noreply.github.com>
2023-08-04 15:25:57 -0400

    Create Meetups.js

--- src/skylab/Meetups.js
+++ src/skylab/Meetups.js
@@ -1,17 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 /* https://h4yyk3ufkjvozw7dflymxogm540drxqp.lambda-url.us-east-2.on.aws/ */
 
 function Meetups() {
-  const [matchmakingData, setMatchmakingData] = useState([]);
+  // State to hold the meetup data
+  const [meetupData, setMeetupData] = useState([]);
 
-    return (
-        <div>
-          <h1>Welcome to Skylab meetups</h1>
-          <p>Hello, world! This is the landing page for Skylab meetups.</p>
-          {/* Add more content or sections as needed */}
-        </div>
+  // Function to fetch meetup data from the Lambda endpoint
+  const fetchMeetupData = async () => {
+    try {
+      const response = await axios.get(
+        'https://h4yyk3ufkjvozw7dflymxogm540drxqp.lambda-url.us-east-2.on.aws/'
       );
+      setMeetupData(response.data);
+    } catch (error) {
+      console.error('Error fetching meetup data:', error);
+    }
+  };
+
+  // Fetch the data when the component mounts
+  useEffect(() => {
+    fetchMeetupData();
+  }, []);
+
+  // JSX to render the table
+  return (
+    <div>
+      <h1>Meetups</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Discord User ID</th>
+            <th>Opted In</th>
+            <th>Ghosted</th>
+            <th>Number of Ghosts</th>
+            <th>Number of Chats</th>
+            <th>Discord Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {meetupData.map((meetup) => (
+            <tr key={meetup.discord_user_id}>
+              <td>{meetup.discord_user_id}</td>
+              <td>{meetup.opted_in.toString()}</td>
+              <td>{meetup.ghosted.toString()}</td>
+              <td>{meetup.num_ghosts}</td>
+              <td>{meetup.num_chats}</td>
+              <td>{meetup.discord_username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Meetups;
